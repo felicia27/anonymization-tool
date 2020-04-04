24f535e4e78ce81543101baed55685078fc09f6f
felicia27 <felicia.hou27@gmail.com>
2020-04-04 14:30:10 -0700

    Fixed Play/Pause Button and Mute/Unmute Button interactions, changed forward/rewind icons

--- staticHTML/edit.js
+++ staticHTML/edit.js
@@ -8,6 +8,7 @@ var wavesurfer = WaveSurfer.create({
     cursorWidth: 1,
     height: 100,
     barGap: 3,
+    skipLength: 5,
     plugins: [ 
         WaveSurfer.cursor.create({
             showTime: true,
@@ -24,15 +25,24 @@ var wavesurfer = WaveSurfer.create({
 
 wavesurfer.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
 wavesurfer.on('ready', function () {
-         wavesurfer.play();
+         //wavesurfer.play();
  });
 
  $('.controls .btn').on('click', function(){
     var action = $(this).data('action');
     console.log(action);
+    var playPauseButton = document.getElementById("playPauseButtonImage")
+    var muteButton = document.getElementById("muteImage")
     switch (action) {
       case 'play':
-        wavesurfer.playPause();
+          if (wavesurfer.isPlaying()){
+            wavesurfer.pause();
+            playPauseButton.src = "image/play-button.png"
+          }
+          else {
+              wavesurfer.play()
+              playPauseButton.src = "image/pause-button.png"
+          }
         break;
       case 'back':
         wavesurfer.skipBackward();
@@ -41,7 +51,17 @@ wavesurfer.on('ready', function () {
         wavesurfer.skipForward();
         break;
       case 'mute':
-        wavesurfer.toggleMute();
+          if (wavesurfer.getMute()){
+            wavesurfer.toggleMute();
+            muteButton.src = "image/unmute.png"
+          }
+          else {
+              wavesurfer.toggleMute()
+              muteButton.src = "image/mute.png"
+          }
+
         break;
     }
   });
+
+ 
